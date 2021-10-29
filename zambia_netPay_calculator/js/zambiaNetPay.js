9c0adfbc3415fdf6357ef7e57a5535b349f9d468
Chimwemwe Shaba <chimwemweshaba@gmail.com>
2021-10-29 21:43:56 +0300

    working1

--- zambia_netPay_calculator/js/zambiaNetPay.js
+++ zambia_netPay_calculator/js/zambiaNetPay.js
@@ -16,22 +16,47 @@ var totalDeductions = 0;
 
 //netPay
 var netPay = 0;
+var bal = 0;
 
-//Constants
-const MAX_NAPSA = 1159.40;
+//CONSTANT
+//Year
 const YEAR = 2021
+
+//Napsa
+const MAX_NAPSA = 1159.40
 const NAPSA_RATE = 0.05;
 
+//NHIS
+const NHIS_RATE = 0.01;
+
+//PAYE
 //Bands
-const BAND1_MAX_VALUE = 3300;
-const BAND2_MAX_VALUE = 4100;
-const BAND3_MAX_VALUE = 6200;
+const BAND1_MAX_VALUE = 4000;
+const BAND2_MAX_VALUE = 4800;
+const BAND3_MAX_VALUE = 6900;
+
+//band values
+var band1_value = 0;
+var band2_value = 0;
+var band3_value = 0;
+var band4_value = 0;
+
+//band tax
+var band1_tax = 0;
+var band2_tax = 0;
+var band3_tax = 0;
+var band4_tax = 0;
 
 //Tax Rates
 const BAND1_TAX_RATE = 0;
 const BAND2_TAX_RATE = 0.25;
 const BAND3_TAX_RATE = 0.30;
 const BAND4_TAX_RATE = 0.375;
+
+//Max Band Tax
+var band1_max_tax = BAND1_MAX_VALUE * BAND1_TAX_RATE;
+var band2_max_tax = (BAND2_MAX_VALUE - BAND1_MAX_VALUE) * BAND2_TAX_RATE;
+var band3_max_tax = (BAND3_MAX_VALUE - BAND2_MAX_VALUE) * BAND3_TAX_RATE;
 
 //initialize
 function initialize()
@@ -74,21 +99,21 @@ function calculate_grossPay(basicPay, housing, transport, otherAllowances)
     this.grossPay = this.basicPay + this.housing + this.transport + this.otherAllowances;
 
     console.log(this.grossPay);
-    return grossPay;
+    return grossPay.toFixed(2);
 }
 
 function get_grossPay()
 {
     console.log(this.grossPay);
-    return this.grossPay;
+    return grossPay.toFixed(2);
 }
 
 //calculate napsa
-function calculate_napsa()
+function calculate_napsa(checkbox_NAPSA)
 {
     if (checkbox_NAPSA.checked == true)
     {
-        this.napsa = this.grossPay * 0.05;
+        this.napsa = this.grossPay * NAPSA_RATE;
 
         if (this.napsa > MAX_NAPSA)
         {
@@ -99,13 +124,51 @@ function calculate_napsa()
     {
         this.napsa = 0;
     }
-    return this.napsa;
+    return napsa.toFixed(2);
+}
+
+//checked paye
+function checked_paye(checkbox_PAYE)
+{
+    if (checkbox_PAYE.checked == true)
+    {
+        calculate_paye();
+    } 
+    else 
+    {
+        this.paye = 0;
+    }
+    return paye.toFixed(2);
 }
 
 //calculate paye
 function calculate_paye()
 {
-    return this.paye;
+    if (grossPay <= BAND1_MAX_VALUE)
+    {
+        band1_value = grossPay;
+        band1_tax = band1_value * BAND1_TAX_RATE;
+        paye = band1_tax;
+    }
+    if (BAND1_MAX_VALUE <= grossPay && grossPay <= BAND2_MAX_VALUE)
+    {
+        band2_value = grossPay - BAND1_MAX_VALUE;
+        band2_tax = band2_value * BAND2_TAX_RATE;
+        paye = band1_max_tax + band2_tax;
+    }
+    else if (BAND2_MAX_VALUE <= grossPay && grossPay <= BAND3_MAX_VALUE)
+    {
+        band3_value = grossPay - BAND2_MAX_VALUE;
+        band3_tax = band3_value * BAND3_TAX_RATE;
+        paye = band1_max_tax + band2_max_tax + band3_tax;
+    }
+    else if (grossPay  > BAND3_MAX_VALUE)
+    {
+        band4_value = grossPay - BAND3_MAX_VALUE;
+        band4_tax = band4_value * BAND4_TAX_RATE;
+        paye = band1_max_tax + band2_max_tax + band3_max_tax + band4_tax;
+    }
+    return paye.toFixed(2);
 }
 
 //calculate nhis
@@ -113,7 +176,7 @@ function calculate_nhis(checkbox_NHIS)
 {
     if (checkbox_NHIS.checked == true)
     {
-        this.nhis = this.basicPay * 0.01;
+        this.nhis = this.basicPay * NHIS_RATE;
     } 
     else 
     {
@@ -122,7 +185,7 @@ function calculate_nhis(checkbox_NHIS)
 
     console.log(this.nhis);
 
-    return nhis;
+    return nhis.toFixed(2);
 }
 
 //set_otherDeductions
@@ -138,7 +201,7 @@ function calculate_totalDeductions()
 {
     this.totalDeductions = this.napsa + this.paye + this.nhis + this.otherDeductions;
     
-    return totalDeductions;
+    return totalDeductions.toFixed(2);
 }
 
 //calculate netPay
@@ -147,7 +210,7 @@ function calculate_netPay()
 
     this.netPay = this.grossPay - this.totalDeductions;
 
-    return netPay;
+    return netPay.toFixed(2);
 }
 
 //submit buttom
