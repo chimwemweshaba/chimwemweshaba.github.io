f1028658c08d4b6b97cc8e12be66840b5f56fc3a
Adam <52896884+chimwemweshaba@users.noreply.github.com>
2021-11-05 10:45:16 +0300

    Update zambiaMotorVehicleClearance.js

--- zambia_motorVehicle_clearance/js/zambiaMotorVehicleClearance.js
+++ zambia_motorVehicle_clearance/js/zambiaMotorVehicleClearance.js
@@ -7,19 +7,19 @@ function get_main_HS_desc(HS_code)
     {
         case 'hs_8702':
         {
-            this.main_HS_desc = "Motor vehicles for the transport of ten or more persons, including the driver. - (BUS)"
+            this.main_HS_desc = "Motor vehicles for the transport of ten or more persons, including the driver."
             break;
         }
 
         case 'hs_8703':
         {
-            this.main_HS_desc = "Motor cars and other motor vehicles principally designed for the transport of persons (other than those of heading 87.02), including station wagons and racing cars. - (Not BUS or TRUCK)"
+            this.main_HS_desc = "Motor cars and other motor vehicles principally designed for the transport of persons (other than those of heading 87.02), including station wagons and racing cars."
             break;
         }
 
         case 'hs_8704':
         {
-            this.main_HS_desc = "Motor vehicles for the transport of goods. - (Truck)"
+            this.main_HS_desc = "Motor vehicles for the transport of goods."
             break;
         }
 
